73047f567f0a74c5eac5e7178a82f7f69a9ea7c1
Javier La Cruz <idsistemas15@gmail.com>
2025-02-19 12:37:36 -0300

    improvements in PrductController

--- src/main/java/com/example/curso/webapp/curso_sb/controllers/ProductController.java
+++ src/main/java/com/example/curso/webapp/curso_sb/controllers/ProductController.java
@@ -2,7 +2,9 @@ package com.example.curso.webapp.curso_sb.controllers;
 
 import com.example.curso.webapp.curso_sb.models.Product;
 import com.example.curso.webapp.curso_sb.services.ProductService;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +17,24 @@ public class ProductController {
   private ProductService productService;
 
   @GetMapping
-  public List<Product> getAllProducts() {
-    return productService.getAllProducts();
+  public ResponseEntity<List<Product>> getAllProducts() {
+    List<Product> products = productService.getAllProducts();
+    if (products.isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204 No Content if empty
+    } else {
+      return new ResponseEntity<>(products, HttpStatus.OK); // 200 OK with the list
+    }
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+  public ResponseEntity<Object> getProductById(@PathVariable Long id) {
     Product product = productService.getProductById(id);
     if (product != null) {
       return new ResponseEntity<>(product, HttpStatus.OK);
     } else {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      Map<String, String> errorResponse = new HashMap<>();
+      errorResponse.put("message", "Product not found");
+      return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
   }
 
@@ -35,7 +44,7 @@ public class ProductController {
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<Product> updateProduct(
+  public ResponseEntity<Object> updateProduct(
     @PathVariable Long id,
     @RequestBody Product product
   ) {
@@ -43,7 +52,9 @@ public class ProductController {
     if (updatedProduct != null) {
       return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     } else {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      Map<String, String> errorResponse = new HashMap<>();
+      errorResponse.put("message", "Product not found");
+      return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
   }
 
